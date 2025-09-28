ba0d0636d7d2df4191e56f737881f941be7c5cd7
Thomas <thomasfred.leonard@gmail.com>
2025-09-28 12:12:37 -0700

    started options

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -10,11 +10,10 @@ func _on_start_pressed():
 	print("Start pressed")
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 
-
-func _on_settings_pressed():
-	print("settings pressed")
-
-
 func _on_exit_pressed():
 	print("exit pressed")
 	get_tree().quit()
+
+
+func _on_options_pressed():
+	pass
