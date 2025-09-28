6d3f32dbfb9c6332bf3ee374d23ebce70e2f1dac
NarrowJS <68929262+NarrowJS@users.noreply.github.com>
2025-09-27 18:02:50 -0700

    added a basic selection ui

--- scripts/texture_progress_bar.gd
+++ scripts/texture_progress_bar.gd
@@ -4,9 +4,13 @@ extends TextureProgressBar
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player.healthChanged.connect(update)
+	print("Player ready: maxHealth =", player.maxHealth, ", currentHealth =", player.currentHealth)
 	update()
+	
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func update():
 	value = player.currentHealth * 100 / player.maxHealth
+	print(player.currentHealth)
